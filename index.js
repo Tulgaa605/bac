bca03922312eecbf48bf2bdb997ff1e37cd39fc5
Tulgaa <munkhtulga0118@gmail.com>
2025-03-22 14:21:42 +0800

    saf

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://fron-sage.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
 app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
